5edcea24b5961c5b20fe6330464b3e9316ac96a1
sankhala <sankhalayash96@gmail.com>
2017-10-07 21:01:55 +0530

    email confirmation feature

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from 'react';
 import {Route} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import HomePage from './components/pages/HomePage'
+import ConfirmationPage from './components/pages/ConfirmationPage'
 import LoginPage from './components/pages/LoginPage'
 import DashboardPage from './components/pages/DashboardPage'
+import SignupPage from './components/pages/SignupPage'
 import UserRoute from './components/routes/UserRoute'
 import GuestRoute from './components/routes/GuestRoute'
 
 
 const App = ({ location }) =>(
     <div className="ui container">
-    <Route location={location} path="/" exact component={HomePage} />
-    <GuestRoute location={location} path="/login" exact component={LoginPage} />
+        <Route location={location} path="/" exact component={HomePage} />
+        <Route location={location} path="/confirmation/:token" exact component={ConfirmationPage} />
+        <GuestRoute location={location} path="/login" exact component={LoginPage} />
+        <GuestRoute location={location} path="/signup" exact component={SignupPage} />
         <UserRoute location={location} path="/dashboard" exact component={DashboardPage} />
 
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import 'semantic-ui-css/semantic.min.css'
 import {createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
 import thunk from 'redux-thunk'
+import decode from 'jwt-decode'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import {BrowserRouter, Route} from 'react-router-dom'
 import App from './App';
@@ -16,7 +17,12 @@ const store = createStore(rootReducer,
 
 
 if(localStorage.bookwormJWT){
-    const user = { token: localStorage.bookwormJWT};
+    const payload = decode(localStorage.bookwormJWT);
+    const user = {
+        token: localStorage.bookwormJWT,
+        email: payload.email,
+        confirmed: payload.confirmed
+        };
     store.dispatch(userLoggedIn(user));
 }
 
